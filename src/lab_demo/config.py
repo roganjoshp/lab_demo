3f2fef4820999f08f4f628941c13889b5e5021c2
Josh Pilkington <j.pilkington@hotmail.com>
2024-01-11 16:35:38 +0000

    Made installable

---
+++ src/lab_demo/config.py
@@ -0,0 +1,42 @@
+MACHINE_STATS = {
+    1: {
+        'ideal_run_rate': 100,
+        'efficiency': 0.85,
+        'min_downtime_secs': 20,
+        'downtime_probability': 0.04,
+        'restart_probability': 0.3
+    },
+    2: {
+        'ideal_run_rate': 100,
+        'efficiency': 0.9,
+        'min_downtime_secs': 120,
+        'downtime_probability': 0.02,
+        'restart_probability': 0.3
+    },
+    3: {
+        'ideal_run_rate': 100,
+        'efficiency': 0.75,
+        'min_downtime_secs': 50,
+        'downtime_probability': 0.01,
+        'restart_probability': 0.3
+    },
+    4: {
+        'ideal_run_rate': 100,
+        'efficiency': 0.6,
+        'min_downtime_secs': 10,
+        'downtime_probability': 0.06,
+        'restart_probability': 0.3
+    }
+}
+
+SALES_FORECAST = {
+    'Product_1': [87, 0, 108, 102],
+    'Product_2': [0, 0, 0, 170],
+    'product_3': [0, 0, 58, 0],
+    'Product_4': [180, 151, 110, 133],
+    'Product_5': [91, 0, 0, 0],
+    'Product_6': [0, 96, 0, 0],
+    'Product_7': [94, 175, 107, 0],
+    'Product_8': [179, 0, 0, 0]
+}
+
