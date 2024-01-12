43629b47cf1cc20a627a8460598563ef5941cde4
Josh Pilkington <j.pilkington@hotmail.com>
2024-01-12 09:08:55 +0000

    Checkpoint

--- src/lab_demo/solver.py
+++ src/lab_demo/solver.py
@@ -1,3 +1,5 @@
+from lab_demo.config import Config
+
 import numpy as np
 import pandas as pd
 
@@ -6,9 +8,11 @@ class Solver:
     
     def __init__(
         self,
-        problem
+        problem,
+        config: Config = Config()
     ):
         self.problem = problem
+        self.config = config
         self._product_names = problem.forecast.columns
         self._forecast = self.problem.forecast.copy()
         
@@ -33,11 +37,27 @@ class Solver:
         # its ideal run rate
         for i, machine in enumerate(self.problem.machines):
             self._productivity_map[i] = np.full(
-                len(self._forecast), 
+                len(self._forecast) - 1, # We don't get the last hour 
                 machine.hourly_production
             )
         
-        df = pd.DataFrame(self._productivity_map) 
-        df.to_csv('productivty_map.csv')   
-    
-    
+        self._forecast.to_csv('forecast.csv')
+        
+        # Now we need to overlay the shift patterns. They need to be expanded
+        # out because they only cover a single week
+        for i, machine in enumerate(self.problem.machines):
+            shift = []
+            week_shift = machine.shift_pattern
+            # First flatten the dictionary to give us a single week
+            for k, v in week_shift.items():
+                shift.extend(v)
+            
+            # Now extend for the total number of weeks
+            num_weeks = int(len(self._forecast) / len(shift))
+            week_shift = shift.copy()
+            for x in range(num_weeks - 1): # We already have the first week
+                shift += week_shift
+            
+            # Now prune down the machine productivity
+            shift = np.array(shift)
+            self._productivity_map[i] = self._productivity_map[i] * shift
